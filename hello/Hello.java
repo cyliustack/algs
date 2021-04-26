811877b5549840bd528ccd5425df70d76f3882a1
james Liu <cyliustack@gmail.com>
2021-04-26 16:32:21 +0000

    add hello project

---
+++ hello/Hello.java
@@ -0,0 +1,12 @@
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Hello {
+
+    public Hello() {
+        System.out.println("Hello, World.");
+    }
+    
+    public static void main(String[] args) {
+       Hello hello = new Hello(); 
+    }
+}
---
+++ hello/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac -cp ./algs4.jar Hello.java
